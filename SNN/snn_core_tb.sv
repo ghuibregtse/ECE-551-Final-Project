29b10d317de29bddc0a12c8393262652ee95b3fc
ghuibregtse <ghuibregtse@wisc.edu>
2018-04-29 12:56:30 -0500

    Update snn_core_tb.sv

--- SNN/snn_core_tb.sv
+++ SNN/snn_core_tb.sv
@@ -3,28 +3,47 @@ module snn_core_tb();
 reg clk, rst_n;
 reg start;
 reg q_input;
-reg [9:0] addr_input_unit;
+reg [9:0] addr_input_unit,addr;
 reg [3:0] digit;
 reg done;
+reg data, we;
 
-ram_input_unit iDUT2(.data(data),.addr(addr_input_unit),.we(we),.clk(clk),.q(q_input));
-snn_core iDUT(.clk(clk), .rst_n(rst_n), .start(start), .q_input(q_input), .addr_input_unit(addr_input_unit), .digit(digit), .done(done));
+ram_input_unit ram(.data(data),.addr(addr_input_unit),.we(we),.clk(clk),.q(q_input));
+snn_core iDUT(.clk(clk), .rst_n(rst_n), .start(start), .q_input(q_input), .addr_input_unit(addr), .digit(digit), .done(done));
 
-initial 
-begin
-addr_input_unit = 0;
-rst_n = 0; //initialize reset as on
-clk = 0;
-start = 0; //initialize start
-@(negedge clk) rst_n = 1; //release reset
-@(negedge clk) start = 1; //start
-while (!done) begin
-@(posedge clk);
-end
-$stop;
+always@(posedge clk, negedge rst_n) begin
+	if(!rst_n)
+		addr_input_unit <= 10'h0;
+	else
+		addr_input_unit <= addr;
 end
 
-always begin
-  #5 clk = ~clk; // clock of period 10 ns, toggling every 5 ns
+initial begin
+	clk = 0;
+	forever 
+		#5 clk = ~clk;
 end
+
+initial begin
+	addr = 0;
+	rst_n = 0;
+	start = 0;
+	done = 0;
+	data = 0;
+	we = 0;
+	digit = 0;
+	@(posedge clk);
+	rst_n = 1;
+	@(posedge clk);
+	@(posedge clk);
+	start = 1;
+	@(posedge clk);
+	start = 1;
+	repeat (1000) @(posedge clk);
+	$stop;
+	
+
+end
+
+
 endmodule
