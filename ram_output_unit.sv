84cc9f312e0aaea594d66cb4d1ebed2d98cc1cff
scohen23 <38566028+scohen23@users.noreply.github.com>
2018-05-01 16:25:48 -0500

    Update ram_output_unit.sv

--- ram_output_unit.sv
+++ ram_output_unit.sv
@@ -12,7 +12,7 @@ module ram_output_unit(data,addr,we,clk,q);
 	input [(DATA_WIDTH-1):0] data;
 	input [(ADDR_WIDTH-1):0] addr;
 	input we, clk;
-	output [(DATA_WIDTH-1):0] q;;
+	output [(DATA_WIDTH-1):0] q;
 	 
 	// Declare the RAM variable
 	reg [DATA_WIDTH-1:0] ram[2**ADDR_WIDTH-1:0];
